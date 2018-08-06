fa750f571ff79ae855214041be730732df157953
Gus Dunn <w.gus.dunn@gmail.com>
2018-08-06 10:27:38 -0400

    add env install to tasks.py

--- tasks.py
+++ tasks.py
@@ -14,11 +14,19 @@ CNAME = 'www.gusdunn.com\ngusdunn.com'
 
 
 @task
+def install(ctx):
+    """Install the needed packages into a virtual environment."""
+    ctx.run("pipenv install")
+
+
+@task
 def clean_deployment_repo(ctx):
     """Clean DEPLOYMENT_REPO of all files, but leave the .git dir and other required things."""
     ctx.run(f"""rm -rf {DEPLOYMENT_REPO}/*""")
     ctx.run(f"""echo '{CNAME}' > {DEPLOYMENT_REPO}/CNAME""")
-    ctx.run(f"""cp {PROJECT_DIR}/README.md {PROJECT_DIR}/LICENSE.html {DEPLOYMENT_REPO}/""")
+    ctx.run(
+        f"""cp {PROJECT_DIR}/README.md {PROJECT_DIR}/LICENSE.html {DEPLOYMENT_REPO}/"""
+    )
 
 
 @task(clean_deployment_repo)
